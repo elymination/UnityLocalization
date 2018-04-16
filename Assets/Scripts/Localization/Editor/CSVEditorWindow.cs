5ec71ef12033b1d8a7485d7a95b7ddf3a1638acf
thehotwaffle <axel.dubillet@gmail.com>
2018-04-16 16:20:42 -0400

    CSVEditorWindow now watches for file modification to update its content.

--- Assets/Scripts/Localization/Editor/CSVEditorWindow.cs
+++ Assets/Scripts/Localization/Editor/CSVEditorWindow.cs
@@ -1,5 +1,6 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEditor;
 using UnityEngine;
 
@@ -8,6 +9,9 @@ public class CSVEditorWindow : EditorWindow
     private static TextAsset mEditedFile;
     private static Dictionary<string, Dictionary<string, string>> mCopiedValues;
     private static Vector2 mScrollPosition;
+    private static FileSystemWatcher mFileSystemWatcher;
+    private static string mResourcesPath;
+    private static string mAssetPath;
 
     public static void LoadFile(TextAsset pFile)
     {
@@ -16,26 +20,35 @@ public class CSVEditorWindow : EditorWindow
 
     public static void ShowWindow()
     {
-        LocalizationService.LoadCSV(AssetDatabase.GetAssetPath(mEditedFile));
+        mResourcesPath = Application.dataPath + "/Resources/";
+        mAssetPath = AssetDatabase.GetAssetPath(mEditedFile);
+        LocalizationService.LoadCSV(mAssetPath);
         mCopiedValues = LocalizationService.GetValues();
         GetWindow<CSVEditorWindow>(false, "CSV Editor", true);
+        mFileSystemWatcher = new FileSystemWatcher();
+        mFileSystemWatcher.Path = mResourcesPath;
+        mFileSystemWatcher.NotifyFilter = NotifyFilters.LastAccess | NotifyFilters.LastWrite | NotifyFilters.FileName | NotifyFilters.DirectoryName;
+        mFileSystemWatcher.Filter = mEditedFile.name + ".csv";
+        mFileSystemWatcher.Changed += OnFileChanged;
+        mFileSystemWatcher.EnableRaisingEvents = true;
+    }
+
+    private static void OnFileChanged(object sender, FileSystemEventArgs e)
+    {
+        LocalizationService.LoadCSV(mAssetPath);
+        mCopiedValues = LocalizationService.GetValues();
     }
 
     void OnGUI()
     {
         List<string> lLanguages = LocalizationService.GetLanguages();
         List<string> lKeys = LocalizationService.GetKeys();
-        if (GUILayout.Button("Update"))
-        {
-            LocalizationService.Update(mCopiedValues);
-        }
         mScrollPosition = EditorGUILayout.BeginScrollView(mScrollPosition);
         GUILayout.BeginHorizontal();
         GUILayout.Label(string.Empty, GUILayout.Width(200));
         foreach (string lLanguage in lLanguages)
         {
             GUILayout.Label(lLanguage, GUILayout.Width(200));
-
         }
         GUILayout.EndHorizontal();
         GUILayout.BeginVertical();
