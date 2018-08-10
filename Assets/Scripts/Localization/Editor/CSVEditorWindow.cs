9e0e3216c2839f66f64bc0d5a03d9dc4f95c6633
thehotwaffle <axel.dubillet@gmail.com>
2018-08-10 11:10:07 -0400

    Modified state added. CSVEditorWindow can now be opened without any file through Window/CSV Editor. File used can be changed within the window.

--- Assets/Scripts/Localization/Editor/CSVEditorWindow.cs
+++ Assets/Scripts/Localization/Editor/CSVEditorWindow.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 
 public class CSVEditorWindow : EditorWindow
 {
+    private static CSVEditorWindow mWindow;
     private static TextAsset mEditedFile;
     private static Dictionary<string, Dictionary<string, string>> mCopiedValues;
     private static Vector2 mScrollPosition;
@@ -16,6 +17,10 @@ public class CSVEditorWindow : EditorWindow
     private static string mKeyName;
     private static List<string> mLanguages;
     private static List<string> mKeys;
+    private static Texture mIcon;
+    private static bool mHasChanged;
+    private static bool mIsDirty;
+    private static bool mFileLoaded;
 
     private static Dictionary<string, Dictionary<string, string>> mAddedKeys;
 
@@ -24,18 +29,26 @@ public class CSVEditorWindow : EditorWindow
         mEditedFile = pFile;
     }
 
+    [MenuItem("Window/CSV Editor")]
     public static void ShowWindow()
     {
-        Texture lIcon = AssetDatabase.LoadAssetAtPath<Texture>("Assets/Resources/CSVEditorIcon.png");
+        mIcon = AssetDatabase.LoadAssetAtPath<Texture>("Assets/Resources/CSVEditorIcon.png");
         mResourcesPath = Application.dataPath + "/Resources/";
-        mAssetPath = AssetDatabase.GetAssetPath(mEditedFile);
+        PrepareWindow("CSV Editor");
+        if (mEditedFile != null)
+        {
+            LoadAsset(mEditedFile);
+        }
+    }
+
+    private static void LoadAsset(TextAsset pAsset)
+    {
+        SetTitle("CSV Editor");
+        mAssetPath = AssetDatabase.GetAssetPath(pAsset);
         LocalizationService.LoadCSV(mAssetPath);
         mLanguages = LocalizationService.GetLanguages();
         mKeys = LocalizationService.GetKeys();
         mCopiedValues = LocalizationService.GetValues();
-        CSVEditorWindow lWindow = GetWindow<CSVEditorWindow>(false, "CSV Editor", true);
-        GUIContent lTitleContent = new GUIContent("CSV Editor", lIcon);
-        lWindow.titleContent = lTitleContent;
         mFileSystemWatcher = new FileSystemWatcher();
         mFileSystemWatcher.Path = mResourcesPath;
         mFileSystemWatcher.NotifyFilter = NotifyFilters.LastAccess | NotifyFilters.LastWrite | NotifyFilters.FileName | NotifyFilters.DirectoryName;
@@ -44,35 +57,52 @@ public class CSVEditorWindow : EditorWindow
         mFileSystemWatcher.EnableRaisingEvents = true;
         mKeyName = string.Empty;
         mAddedKeys = new Dictionary<string, Dictionary<string, string>>();
+        mHasChanged = false;
+        mIsDirty = false;
+        mFileLoaded = true;
+    }
+
+    private static void PrepareWindow(string pTitle)
+    {
+        mWindow = GetWindow<CSVEditorWindow>(false, pTitle, true);
+    }
+
+    private static void SetTitle(string pTitle)
+    {
+        if (mWindow != null)
+        {
+            GUIContent lTitleContent = new GUIContent(pTitle, mIcon);
+            mWindow.titleContent = lTitleContent;
+        }
     }
 
     private static void OnFileChanged(object sender, FileSystemEventArgs e)
     {
-        bool lFileIsLocked = true;
-        while (lFileIsLocked == true)
-        {
-            try
-            {
-                // Attempt to open the file exclusively.
-                using (FileStream fs = new FileStream(mAssetPath, FileMode.Open, FileAccess.ReadWrite, FileShare.None, 100))
-                {
-                    fs.ReadByte();
-                    lFileIsLocked = false;
-                }
-            }
-            finally
-            {
-                System.Threading.Thread.Sleep(50);
-            }
-        }
+        WaitUntilWritable();
         LocalizationService.LoadCSV(mAssetPath);
         mLanguages = LocalizationService.GetLanguages();
         mKeys = LocalizationService.GetKeys();
         mCopiedValues = LocalizationService.GetValues();
+        if (mWindow != null)
+        {
+            mWindow.Repaint();
+        }
     }
 
     void OnGUI()
     {
+        if (mFileLoaded == false)
+        {
+            GUILayout.FlexibleSpace();
+            GUILayout.BeginHorizontal();
+            mEditedFile = EditorGUILayout.ObjectField(mEditedFile, typeof(TextAsset), false, GUILayout.MinWidth(150), GUILayout.MaxWidth(300), GUILayout.ExpandWidth(false)) as TextAsset;
+            if (GUILayout.Button("Load", GUILayout.Width(200)) == true)
+            {
+                LoadAsset(mEditedFile);
+            }
+            GUILayout.EndHorizontal();
+            return;
+        }
         string lPoppedTempKey = string.Empty;
         string lPoppedKey = string.Empty;
         mScrollPosition = EditorGUILayout.BeginScrollView(mScrollPosition);
@@ -93,6 +123,8 @@ public class CSVEditorWindow : EditorWindow
             if (GUILayout.Button("-", GUILayout.Width(15)) == true)
             {
                 lPoppedKey = lKey;
+                SetTitle("CSV Editor*");
+                mHasChanged = true;
             }
             GUILayout.Label(lKey, GUILayout.Width(200));
 
@@ -143,24 +175,58 @@ public class CSVEditorWindow : EditorWindow
         {
             mAddedKeys.Remove(lPoppedTempKey);
         }
+
         GUILayout.BeginHorizontal();
-        mKeyName = GUILayout.TextField(mKeyName, GUILayout.Width(200));
-        if (GUILayout.Button("Add", GUILayout.Width(50)) == true)
+        if (GUI.changed == true)
         {
-            CreateKey();
+            mHasChanged = true;
         }
 
-        GUILayout.EndHorizontal();
-        if (GUILayout.Button("Save CSV", GUILayout.Width(200)) == true)
+        mKeyName = GUILayout.TextField(mKeyName, GUILayout.Width(200), GUILayout.Height(15));
+
+        if (GUILayout.Button("Add", GUILayout.Width(100), GUILayout.Height(15)) == true)
         {
-            RefreshCSV();
-            WriteNewValues();
+            if (CreateKey() == true)
+            {
+                SetTitle("CSV Editor*");
+                mHasChanged = true;
+            }
         }
+
+        if (mHasChanged == true)
+        {
+            if (GUILayout.Button("Save CSV", GUILayout.Width(200)) == true)
+            {
+                RefreshCSV();
+                WriteNewValues();
+                SetTitle("CSV Editor");
+                mHasChanged = false;
+            }
+        }
+        GUILayout.EndHorizontal();
+        GUILayout.BeginVertical();
+        GUILayout.Space(10);
+        GUILayout.EndVertical();
+        if (mIsDirty == true)
+        {
+            AssetDatabase.Refresh();
+            mIsDirty = false;
+        }
+
+        GUI.changed = false;
+        GUILayout.BeginHorizontal();
+        mEditedFile = EditorGUILayout.ObjectField(mEditedFile, typeof(TextAsset), false, GUILayout.MinWidth(150), GUILayout.MaxWidth(300), GUILayout.ExpandWidth(false)) as TextAsset;
+        if (GUILayout.Button("Load", GUILayout.Width(200)) == true)
+        {
+            LoadAsset(mEditedFile);
+        }
+        GUILayout.EndHorizontal();
     }
 
     // Write the existing keys (and remove the deleted ones) to the CSV file.
     private void RefreshCSV()
     {
+        WaitUntilWritable();
         using (StreamWriter sw = new StreamWriter(mAssetPath))
         {
             string lFullText = "Key;";
@@ -195,6 +261,7 @@ public class CSVEditorWindow : EditorWindow
     // Write the new values to the CSV file.
     private void WriteNewValues()
     {
+        WaitUntilWritable();
         using (StreamWriter sw = File.AppendText(mAssetPath))
         {
             bool lHasEmptyKey = false;
@@ -226,19 +293,19 @@ public class CSVEditorWindow : EditorWindow
             }
             if (lHasEmptyKey == false && lFullText.Length > 0)
             {
-                //lFullText = lFullText.Substring(0, lFullText.Length - 1);
                 sw.Write(lFullText);
                 mAddedKeys.Clear();
             }
         }
+        mIsDirty = true;
     }
 
     // Create new key
-    private void CreateKey()
+    private bool CreateKey()
     {
         if (mKeyName.Equals(string.Empty) || mKeys.Contains(mKeyName) || mAddedKeys.Keys.Contains(mKeyName))
         {
-            return;
+            return false;
         }
         Dictionary<string, string> lDictionary = new Dictionary<string, string>();
         foreach (string lLanguage in mLanguages)
@@ -247,5 +314,27 @@ public class CSVEditorWindow : EditorWindow
         }
         mAddedKeys.Add(mKeyName, lDictionary);
         mKeyName = string.Empty;
+        return true;
+    }
+
+    private static void WaitUntilWritable()
+    {
+        bool lFileIsLocked = true;
+        while (lFileIsLocked == true)
+        {
+            try
+            {
+                // Attempt to open the file exclusively.
+                using (FileStream fs = new FileStream(mAssetPath, FileMode.Open, FileAccess.ReadWrite, FileShare.None, 100))
+                {
+                    fs.ReadByte();
+                    lFileIsLocked = false;
+                }
+            }
+            finally
+            {
+                System.Threading.Thread.Sleep(50);
+            }
+        }
     }
 }
